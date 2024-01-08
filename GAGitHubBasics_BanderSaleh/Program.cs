8a448767e02d83b516fc6a6ba7de2dfd373b779f
Bander Saleh <bandersaleh93@gmail.com>
2024-01-08 13:34:07 -0800

    Added 2nd console.writeline msg about what I want to learn in computer science

--- GAGitHubBasics_BanderSaleh/Program.cs
+++ GAGitHubBasics_BanderSaleh/Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Professor");
+Console.WriteLine("My name is Bander Saleh and I would like to learn coding algorithms like how to sort a list.");
